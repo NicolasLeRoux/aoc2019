5cfdbfa3ba05505cea41e1d89e76a88da648999b
Nicolas <mr.leroux.nicolas@gmail.com>
2019-12-04 19:25:49 +0100

    feat(Day04): Solve part 2 puzzle

--- days/04/solver/solver.go
+++ days/04/solver/solver.go
@@ -28,3 +28,48 @@ func SolvePartOne(min int, max int) int {
 
 	return len(list)
 }
+
+// SolvePartTwo is the main solver of the day 04 part 2 puzzle
+func SolvePartTwo(min int, max int) int {
+	var list []int
+
+	for i := min; i <= max; i++ {
+		nb := strconv.Itoa(i)
+		digit1, err1 := strconv.Atoi(string(nb[0]))
+		digit2, err2 := strconv.Atoi(string(nb[1]))
+		digit3, err3 := strconv.Atoi(string(nb[2]))
+		digit4, err4 := strconv.Atoi(string(nb[3]))
+		digit5, err5 := strconv.Atoi(string(nb[4]))
+		digit6, err6 := strconv.Atoi(string(nb[5]))
+
+		if err1 != nil || err2 != nil || err3 != nil || err4 != nil || err5 != nil || err6 != nil {
+			panic("Cannot parse number !")
+		}
+
+		if digit1 <= digit2 && digit2 <= digit3 && digit3 <= digit4 && digit4 <= digit5 && digit5 <= digit6 && (digit1 == digit2 || digit2 == digit3 || digit3 == digit4 || digit4 == digit5 || digit5 == digit6) {
+			pairs := 0
+
+			if digit1 == digit2 && digit2 != digit3 {
+				pairs++
+			}
+			if digit1 != digit2 && digit2 == digit3 && digit3 != digit4 {
+				pairs++
+			}
+			if digit2 != digit3 && digit3 == digit4 && digit4 != digit5 {
+				pairs++
+			}
+			if digit3 != digit4 && digit4 == digit5 && digit5 != digit6 {
+				pairs++
+			}
+			if digit4 != digit5 && digit5 == digit6 {
+				pairs++
+			}
+
+			if pairs >= 1 {
+				list = append(list, i)
+			}
+		}
+	}
+
+	return len(list)
+}
--- days/04/solver/solver_test.go
+++ days/04/solver/solver_test.go
@@ -31,3 +31,34 @@ func TestSolvePartOne(t *testing.T) {
 		})
 	}
 }
+
+func TestSolvePartTwo(t *testing.T) {
+	suites := []struct {
+		description string
+		input1      int
+		input2      int
+		expected    int
+	}{
+		{"TODO", 112233, 112233, 1},
+		{"TODO", 123444, 123444, 0},
+		{"TODO", 122444, 122444, 1},
+		{"TODO", 111122, 111122, 1},
+		{"TODO", 111223, 111223, 1},
+		{"TODO", 112223, 112223, 1},
+		{"TODO", 122234, 122234, 0},
+	}
+
+	for _, suite := range suites {
+		t.Run(suite.description, func(t *testing.T) {
+			output := SolvePartTwo(suite.input1, suite.input2)
+
+			if output != suite.expected {
+				t.Errorf("TestSolvePartTwo(%d, %d) got %d but expect %d",
+					suite.input1,
+					suite.input2,
+					output,
+					suite.expected)
+			}
+		})
+	}
+}
